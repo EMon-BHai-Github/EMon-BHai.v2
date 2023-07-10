0ea39bb93d748b96dd70e81ac8ec840e9f37e28f
EMon-BHai PROJECT <138991689+EMon-BHai-Github@users.noreply.github.com>
2023-07-10 16:06:09 +0800

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -3,10 +3,10 @@ const chalk = require('chalk');
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.yellow('[ ❕  It's wrong ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.red('[ ❕ It's wrong ] » ') + data);
 			break;
 		default:
 			console.log(chalk.magenta(`${option} » `) + data);
@@ -17,13 +17,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[D-JUKIE ] » ') + data);
+			console.log(chalk.yellow('[EMon-BHai ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[D-JUKIE ] » ') + data);
+			console.log(chalk.red('[EMon-BHai ] » ') + data);
 			break;
 		default:
-			console.log(chalk.green(`[D-JUKIE ] » `) + data);
+			console.log(chalk.green(`[EMon-BHai ] » `) + data);
 			break;
 	}
-}
+						 }
